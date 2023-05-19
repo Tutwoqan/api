4688d598836fdcf7a747782de64f9b1c83910095
Andrew Golovashevich <tankist.scratch.p@gmail.com>
2023-05-19 19:18:49 +0300

    Initial commit

---
+++ build.sh
@@ -0,0 +1,6 @@
+kotlinc-jvm                                                                                                            \
+    -d out/api.jar                                                                                                     \
+    -jvm-target "1.8"                                                                                                  \
+    -module-name totwoqan-api                                                                                          \
+    -language-version 1.7                                                                                              \
+    -Xallow-no-source-files # temp
